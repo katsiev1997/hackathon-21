3407a735a714519f46ad41b9a07e9e12d6fe8c28
katsiev1997 <katsiev1997@gmail.com>
2026-04-14 21:36:16 +0300

    add vite-bundle-analyzer for improved bundle visualization and add build:debug script

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,13 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { analyzer } from "vite-bundle-analyzer";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), preact()],
+  plugins: [tailwindcss(), preact(), analyzer()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
