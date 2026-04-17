300015c308c4128c65e32c433847d664ab97df55
katsiev1997 <katsiev1997@gmail.com>
2026-04-17 17:01:15 +0300

    enhance pre-commit hook for biome checks and update dependencies; add sonner for notifications in create team feature

--- react-router-ssr/app/features/create-team/ui/create-team-button.tsx
+++ react-router-ssr/app/features/create-team/ui/create-team-button.tsx
@@ -1,27 +1,39 @@
 import { Plus } from "lucide-react";
 import { useState } from "react";
+import { toast } from "sonner";
 import type { TeamCardData } from "~/entities/team/model/types";
+import { useGetProfile } from "~/entities/user";
 import { CreateTeamDialog } from "~/features/create-team/ui/create-team-dialog";
 import { Button } from "~/shared/components/ui/button";
 
 type CreateTeamButtonProps = {
-	onTeamCreated?: (team: TeamCardData) => void;
+  onTeamCreated?: (team: TeamCardData) => void;
 };
 
 export function CreateTeamButton({ onTeamCreated }: CreateTeamButtonProps) {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { data } = useGetProfile();
+  const { lookingForTeam } = data ?? {};
 
-	return (
-		<>
-			<Button type="button" size="default" onClick={() => setOpen(true)}>
-				<Plus data-icon="inline-start" />
-				Create Team
-			</Button>
-			<CreateTeamDialog
-				open={open}
-				onOpenChange={setOpen}
-				onTeamCreated={onTeamCreated}
-			/>
-		</>
-	);
+  const handleClick = () => {
+    if (lookingForTeam) {
+      () => setOpen(true);
+    } else {
+      toast.error("У вас уже есть команда");
+    }
+  };
+
+  return (
+    <>
+      <Button type="button" size="default" onClick={handleClick}>
+        <Plus data-icon="inline-start" />
+        Create Team
+      </Button>
+      <CreateTeamDialog
+        open={open}
+        onOpenChange={setOpen}
+        onTeamCreated={onTeamCreated}
+      />
+    </>
+  );
 }
